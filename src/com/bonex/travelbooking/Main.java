2874df592bee4c378c62024875f08a43fe70ae98
Bokros Csaba benti gép <bokroscsaba@gmail.com>
2022-08-11 16:31:45 +0200

    Ticket

--- src/com/bonex/travelbooking/Main.java
+++ src/com/bonex/travelbooking/Main.java
@@ -1,4 +1,13 @@
 package com.bonex.travelbooking;
 
+import com.bonex.travelbooking.domain.TrainTicket;
+
 public class Main {
+    public static void main(String[] args) {
+
+        TrainTicket trainTicket = new TrainTicket();
+        trainTicket.cancel();
+
+    }
+
 }
--- src/com/bonex/travelbooking/domain/TrainTicket.java
+++ src/com/bonex/travelbooking/domain/TrainTicket.java
@@ -2,6 +2,31 @@ package com.bonex.travelbooking.domain;
 
 public class TrainTicket extends TravelTicket {
 
+    private Integer travelClass;
+    private Integer carriageNumber;
+    private Integer seatNumber;
 
+    public Integer getTravelClass() {
+        return travelClass;
+    }
 
+    public void setTravelClass(Integer travelClass) {
+        this.travelClass = travelClass;
+    }
+
+    public Integer getCarriageNumber() {
+        return carriageNumber;
+    }
+
+    public void setCarriageNumber(Integer carriageNumber) {
+        this.carriageNumber = carriageNumber;
+    }
+
+    public Integer getSeatNumber() {
+        return seatNumber;
+    }
+
+    public void setSeatNumber(Integer seatNumber) {
+        this.seatNumber = seatNumber;
+    }
 }
