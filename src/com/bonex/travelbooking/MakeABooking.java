bb2411fd024ee63b451af468166f5009d6a2d0dc
Your NameÉn <bokroscsaba@gmail.com>
2022-08-16 22:00:34 +0200

    travel

--- src/com/bonex/travelbooking/MakeABooking.java
+++ src/com/bonex/travelbooking/MakeABooking.java
@@ -2,6 +2,8 @@ package com.bonex.travelbooking;
 
 import com.bonex.travelbooking.domain.BusTicket;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class MakeABooking {
@@ -19,6 +21,25 @@ public class MakeABooking {
                 System.out.println("Type in booking reference:");
                 Long bf = scanner.nextLong();
                 busticket.setBookingRef(bf);
+
+                System.out.println("Type in booking origin:");
+                String origin = scanner.nextLine();
+                busticket.setOrigin(origin);
+
+                System.out.println("Type in booking destination:");
+                String destination = scanner.nextLine();
+                busticket.setDestination(destination);
+
+                System.out.println("Type in departure time (yyyy-MM-dd HH:mm):");
+                String departureTime = scanner.nextLine();
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+                LocalDateTime dateTime = LocalDateTime.parse(departureTime, formatter);
+                busticket.setDepartureTime(dateTime);
+
+
+
+
+
         }
 
 
