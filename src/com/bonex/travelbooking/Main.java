7fd6209a1f9657a1f9a363c3ef2a0660c246a556
Your NameÉn <bokroscsaba@gmail.com>
2022-09-01 11:13:06 +0200

    travel

--- src/com/bonex/travelbooking/Main.java
+++ src/com/bonex/travelbooking/Main.java
@@ -7,9 +7,10 @@ import com.bonex.travelbooking.domain.TravelDurationException;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws TravelDurationException {
 
         TrainTicket trainTicket = new TrainTicket();
         trainTicket.cancel();
@@ -24,20 +25,45 @@ public class Main {
 
 
         trainTicket2.upgrade();
-        String[] providers = {"1st bus company", "2nd bus company", "3rd bus company"};
+//        String[] providers = {"1st bus company", "2nd bus company", "3rd bus company"};
+        ArrayList<String> providers = new ArrayList<>();
+        providers.add("Első vállalat");
+        providers.add("Második válallat");
+        providers.add("Harmadik vállalat");
+
+        System.out.println(providers);
 
         BusTicket busTicket = null;
-        try {
-            busTicket = new BusTicket(123L, "London", "Edinburgh", new BigDecimal("54.32"),
+
+        busTicket = new BusTicket(123L, "London", "Edinburgh", new BigDecimal("54.32"),
                     LocalDateTime.of(2022, 3, 7, 16, 03),
                     LocalDateTime.of(2023, 3, 7, 19, 03), providers);
                     busTicket.cancel();
-        } catch (TravelDurationException e) {
-            System.out.println("Hiba történt" + e);
+
+        BusTicket busTicket2 = new BusTicket(124L, "London", "Edinburgh", new BigDecimal("54.32"),
+                LocalDateTime.of(2022, 3, 7, 16, 03),
+                LocalDateTime.of(2023, 3, 7, 19, 03), providers);
+        busTicket.cancel();
+
+
+        BusTicket busTicket3 = new BusTicket(125L, "London", "Edinburgh", new BigDecimal("54.32"),
+                LocalDateTime.of(2022, 3, 7, 16, 03),
+                LocalDateTime.of(2023, 3, 7, 19, 03), providers);
+        busTicket.cancel();
+
+
+
+        ArrayList<BusTicket> busTickets = new ArrayList<>();
+        busTickets.add(busTicket);
+        busTickets.add(busTicket2);
+        busTickets.add(busTicket3);
+
+//        for (int i=0; i<busTickets.size(); i++){
+//            System.out.println(busTickets.get(i));
+//        }
+        for (BusTicket b: busTickets){
+            System.out.println(b);
         }
-
-
-
     }
 
 }
--- src/com/bonex/travelbooking/domain/BusTicket.java
+++ src/com/bonex/travelbooking/domain/BusTicket.java
@@ -3,25 +3,27 @@ package com.bonex.travelbooking.domain;
 import java.math.BigDecimal;
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Objects;
 
 public class BusTicket extends TravelTicket {
 
-    private String[] permittedProviders;
+    private ArrayList<String> permittedProviders = new ArrayList<>();
     public BusTicket() {
         super();
     }
     public BusTicket(Long bookingRef, String origin, String destination, BigDecimal price,
-                     LocalDateTime departureTime, LocalDateTime arrivalTime, String[] permittedProviders) throws TravelDurationException {
+                     LocalDateTime departureTime, LocalDateTime arrivalTime, ArrayList<String> permittedProviders) throws TravelDurationException {
         super(bookingRef, origin, destination, price, departureTime, arrivalTime);
         this.permittedProviders = permittedProviders;
     }
 
-    public String[] getPermittedProviders() {
+    public ArrayList<String> getPermittedProviders() {
         return permittedProviders;
     }
 
-    public void setPermittedProviders(String[] permittedProviders) {
+    public void setPermittedProviders(ArrayList<String> permittedProviders) {
         this.permittedProviders = permittedProviders;
 
     }
@@ -42,12 +44,10 @@ public class BusTicket extends TravelTicket {
 
     @Override
     public String toString() {
-        return "BusTicket{" +
-                "permittedProviders=" + Arrays.toString(permittedProviders) +
-                '}';
+        return "BusTicket{" + super.toString() +
+                "permittedProviders=" + permittedProviders +
+                "} " ;
     }
-
-
 
     @Override
     public boolean equals(Object o) {
@@ -55,14 +55,11 @@ public class BusTicket extends TravelTicket {
         if (o == null || getClass() != o.getClass()) return false;
         if (!super.equals(o)) return false;
         BusTicket busTicket = (BusTicket) o;
-        return Arrays.equals(permittedProviders, busTicket.permittedProviders);
+        return Objects.equals(permittedProviders, busTicket.permittedProviders);
     }
 
     @Override
     public int hashCode() {
-        int result = super.hashCode();
-        result = 31 * result + Arrays.hashCode(permittedProviders);
-        return result;
+        return Objects.hash(super.hashCode(), permittedProviders);
     }
-
 }
